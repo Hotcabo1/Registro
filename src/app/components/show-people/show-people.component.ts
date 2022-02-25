a42120316976bc6787cabd648b929f4b81385af0
Hotcabo1-From Ramses <carlosceravargas@gmail.com>
2022-02-25 17:02:50 -0600

    Delete (|C|R| U |D|) Angular

--- src/app/components/show-people/show-people.component.ts
+++ src/app/components/show-people/show-people.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CrudService } from 'src/app/servicio/crud.service';
+
+import Swal from 'sweetalert2';
+
+
 @Component({
   selector: 'app-show-people',
   templateUrl: './show-people.component.html',
   styleUrls: ['./show-people.component.css']
 })
 export class ShowPeopleComponent implements OnInit {
+  
+  Empleados:any;
 
-  constructor() { }
+  constructor(
+    private crudService:CrudService
+  ) { }
 
   ngOnInit(): void {
+    this.crudService.ObtenerE().subscribe(resp=>{
+      console.log(resp);
+      this.Empleados=resp;
+    });
+  }
+  borrarRegistro(id:any,iControl:any){
+    console.log(id);
+    console.log(iControl);
+
+    this.crudService.EliminarEmpleado(id).subscribe((resp)=>{
+      this.Empleados.splice(iControl,1);
+      Swal.fire('¡Empleado eliminado!');
+    });
+    
+
   }
 
 }
--- src/app/servicio/crud.service.ts
+++ src/app/servicio/crud.service.ts
@@ -10,11 +10,22 @@ import { Empleado } from './Empleado';
 })
 // --------------CRUD SERVICE-------------- //
 export class CrudService {
-API: string="http://localhost/empleados-main/"
+
+  API: string="http://localhost/empleados-main/"; // Api proveniente del doc de PHP corriendo en servidor xampp
+
   constructor(private clienteHttp:HttpClient) { }
 
   AgregarEmpleado(datosEmpleado:Empleado):Observable<any>{
     return this.clienteHttp.post(this.API+"?insertar=1", datosEmpleado);
   }
+
+  ObtenerE(){
+    return this.clienteHttp.get(this.API);
+  }
+
+  EliminarEmpleado(id:Empleado):Observable<any>{
+    return this.clienteHttp.get(this.API+"?borrar="+1);
+  }
+
 }
 // --------------CRUD SERVICE-------------- //
